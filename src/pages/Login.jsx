1a8989b7f7ea061f84b33c6152ff49a32afa0f4e
Shanella <140229356+ShanellaChr@users.noreply.github.com>
2025-11-28 17:14:18 +0700

    Test Color
    Coba add warna ke tailwind, masih gagal dipakai lgsg utility nya

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,14 +1,20 @@
 export default function Login() {
   return (
-    <div className="p-6">
-      <h1 className="text-3xl  text-(--color-SMnavy)">Login ini</h1>
     <div>
-      <p className="text-black text-5xl font-montserrat">halo montserrat</p>
-      <p className="text-black text-5xl font-poppins font-normal">halo poppins</p>
-      <p className="text-black text-5xl font-poppins font-medium">halo poppins</p>
-      <p className="text-black text-5xl font-poppins font-semibold">halo poppins</p>
-      
-    </div>
+      <p className="text-SMnavy">TEST WARNA TELWIND</p>
+      <p className="text-(--color-SMredAlert) font-bold">
+        hanjay kalo gini keluar warnanya, tapi kan repot panjang..
+        <br />dibikinin tailwind.config.js gajalan asw 
+      </p>
+
+      <div className="min-h-screen flex items-center justify-center">
+        <div className="w-220 h-160 bg-blue-400 rounded-2xl shadow-lg">
+          {/* isi di dalam backdrop */}
+          <h1 className="font-montserrat text-SMtsel">TEST MONTSERRAT dan warna?</h1>
+          <p className="font-poppins text-SMnavy">TEST Poppins dan warna?</p>
+
+        </div>
+      </div>
     </div>
   )
 }
