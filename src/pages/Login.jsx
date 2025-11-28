3e1395a578c7c9128ac33ffe0c5db634a9f557dd
Shanella <140229356+ShanellaChr@users.noreply.github.com>
2025-11-28 15:44:41 +0700

    add Font Type
    Testing Font to Tailwind

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,14 @@
 export default function Login() {
   return (
     <div className="p-6">
-      <h1 className="text-3xl font-[var(--font-montserrat)] text-red-500">Login ini</h1>
+      <h1 className="text-3xl  text-(--color-SMnavy)">Login ini</h1>
+    <div>
+      <p className="text-black text-5xl font-montserrat">halo montserrat</p>
+      <p className="text-black text-5xl font-poppins font-normal">halo poppins</p>
+      <p className="text-black text-5xl font-poppins font-medium">halo poppins</p>
+      <p className="text-black text-5xl font-poppins font-semibold">halo poppins</p>
+      
+    </div>
     </div>
   )
 }
