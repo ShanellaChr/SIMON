9be18834d533357cc868872de756da972539cddd
Shanella <140229356+ShanellaChr@users.noreply.github.com>
2025-12-02 13:47:36 +0700

    Done set tailwind
    tailwind add color and font sudah lancar boi

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,19 +1,14 @@
 export default function Login() {
   return (
-    <div>
-      <p className="text-SMnavy">TEST WARNA TELWIND</p>
-      <p className="text-(--color-SMredAlert) font-bold">
-        hanjay kalo gini keluar warnanya, tapi kan repot panjang..
-        <br />dibikinin tailwind.config.js gajalan asw 
-      </p>
+    <div className="flex items-center justify-center h-screen">
+      <div className="w-220 h-160 bg-SMaqua rounded-xl drop-shadow-2xl">
+        <h1 className="font-montserrat text-SMtsel">
+          TEST MONTSERRAT dan warna?
+        </h1>
 
-      <div className="min-h-screen flex items-center justify-center">
-        <div className="w-220 h-160 bg-blue-400 rounded-2xl shadow-lg">
-          {/* isi di dalam backdrop */}
-          <h1 className="font-montserrat text-SMtsel">TEST MONTSERRAT dan warna?</h1>
-          <p className="font-poppins text-SMnavy">TEST Poppins dan warna?</p>
-
-        </div>
+        <p className="font-poppins text-SMnavy">
+          TEST Poppins dan warna?
+        </p>
       </div>
     </div>
   )
